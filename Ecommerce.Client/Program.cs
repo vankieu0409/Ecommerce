c5d1993fcea22fa384022dc32fc2e00fd93f5df8
kieu-rp <kieunv@reactplus.com>
2024-10-10 09:33:45 +0700

    Commit

--- Ecommerce.Client/Program.cs
+++ Ecommerce.Client/Program.cs
@@ -5,8 +5,6 @@ using Ecommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using MudBlazor.Services;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -14,6 +12,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddMudServices();
+builder.Services.AddAntDesign();
+
 
 await builder.Build().RunAsync();
