4e05a4dc9303728ae321e7f72897769a01461a96
kieu-rp <kieunv@reactplus.com>
2024-10-11 17:44:08 +0700

    dự base

---
+++ Servers/Ecommerce.Domain/Entities/Author/Customer.cs
@@ -0,0 +1,8 @@
+﻿using Ecommerce.Shared.Domains;
+
+namespace Ecommerce.Domain.Entities.Author;
+
+public class Customer : EntityAuditBase<Guid>
+{
+
+}
---
+++ Servers/Ecommerce.Domain/Entities/Author/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Shared.Domains;
+
+namespace Ecommerce.Domain.Entities.Author;
+
+public class Employee : EntityAuditBase<Guid>
+{
+    public string SocialSecurityNumber { get; set; }
+    public string FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string PlaceOfOrigin { get; set; }
+    public string ResidenceAddress { get; set; }
+    public string DistinctiveFeatures { get; set; }
+    public DateTime IssueDate { get; set; }
+    public string Avatar { get; set; }
+    public string Nationality { get; set; }
+    public string PasswordHash { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
